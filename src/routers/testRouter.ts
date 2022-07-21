cbcc62a5014a83a85308bb2a37aa8392b1b2dd49
Lucas Viana <lucasvfv@gmail.com>
2022-07-21 13:03:58 -0300

    fix: test router added schema validate

--- src/routers/testRouter.ts
+++ src/routers/testRouter.ts
@@ -1,9 +1,11 @@
+import { testSchema } from './../schemas/schemas.js';
 import { Router } from "express";
 import { createTest } from "../controllers/testControllers.js";
+import { schemaValidate } from "../middlewares/validateSchema.js";
 
 const testRouter = Router();
 
-testRouter.post("/test", createTest)
+testRouter.post("/test", schemaValidate(testSchema), createTest)
 
 
 export default testRouter
